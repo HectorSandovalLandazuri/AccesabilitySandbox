7139d93fa8c91679d76ac48903249854627992fb
Héctor Sandoval <ingsan@live.com.mx>
2023-10-30 09:53:06 -0600

    Initial Commit

---
+++ AccesabilitySandbox/AccesabilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccesabilitySandboxApp.swift
+//  AccesabilitySandbox
+//
+//  Created by Héctor Manuel Sandoval Landázuri on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AccesabilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
